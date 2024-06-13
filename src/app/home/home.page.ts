78c0245ab7eb20d77c59859f5c13be2af89fb1fa
salem <kammoun.salem@gmail.com>
2024-06-13 09:13:49 +0100

    resolution de bug in-app-browser

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+//import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { InAppBrowser } from '@capgo/inappbrowser'
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,16 @@ import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 })
 export class HomePage {
 
-  constructor(private iab:InAppBrowser,
+  constructor(
 
 ) {}
 
 ionViewWillEnter() {
   var options = "width=437,height=250,location=no,zoom=no,keyboardDisplayRequiresUserAction=no,toolbar=no,toolbarposition=top,closebuttoncaption=fermer,usewkwebview=yes";
-  let browser = this.iab.create("http://192.168.1.11/DEMO", "_blank",options);
-  browser.show()
+
+  InAppBrowser.open({ url: "http://google.com" });
+
+
 }
 
 }
